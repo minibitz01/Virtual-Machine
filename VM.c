c081fcf61923926847154cbc3af36ed1d911c854
Ana <139091103+minibitz01@users.noreply.github.com>
2026-03-05 21:30:30 -0500

    Update VM.c

--- VM.c
+++ VM.c
@@ -1,26 +1,3 @@
-/*
-Assignment:
-vm.c - Implement a P-machine virtual machine
-Authors: Naseem Elbram and Ana Paula Ramos Lopez 
-Language: C (only)
-To Compile:
-gcc -O2 -Wall -std=c11 -o vm vm.c
-To Execute (on Eustis):
-./vm input.txt
-where:
-input.txt is the name of the file containing PM/0 instructions;
-each line has three integers (OP L M)
-Notes:
-- Implements the PM/0 virtual machine described in the homework
-instructions.
-- No dynamic memory allocation, no pointer arithmetic, and
-no function-like macros.
-- Does not implement any VM instruction using a separate function.
-- Runs on Eustis.
-Class: COP 3402 - Systems Software - Spring 2026
-Instructor : Dr. Jie Lin
-Due Date: Monday, February 9th, 2026
-*/
 #include <stdio.h>
 #include <stdlib.h>
 #define PAS_SIZE 500
